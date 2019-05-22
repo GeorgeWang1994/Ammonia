3db4f9a1405c0458ef58761bcf61e5b47a5964fc
George <georgewang1994@163.com>
2019-05-22 09:42:45 +0800

    fix

--- ammonia/tests/test_pool.py
+++ ammonia/tests/test_pool.py
@@ -10,17 +10,14 @@
 """
 
 import asyncio
+import time
 
 from ammonia.backends.worker.pool import AsyncPool
 
 
-async def example_coro(initial_number, result_queue):
-    print("Processing Value! -> {} * 2 = {}".format(initial_number, initial_number * 2))
-    await asyncio.sleep(1)
-    await result_queue.put(initial_number * 2)
-
-
 def result_executor(i):
+    print("%s: 等待1s" % i)
+    time.sleep(1)
     return i
 
 
@@ -33,9 +30,7 @@ def fail_executor(i):
 
 
 async def run():
-
-    # Start a worker pool with 10 coroutines, invokes `example_coro` and waits for it to complete or 5 minutes to pass.
-    async with AsyncPool(worker_count=2) as pool:
+    async with AsyncPool(worker_count=10) as pool:
         for i in range(50):
             await pool.apply_async(result_executor, success_executor, fail_executor, i)
 
